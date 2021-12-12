c51906a519e42b41ce38a5d088ebd788e553b8f3
jvsena42 <jvsena16@gmail.com>
2021-12-12 11:40:55 -0300

    reduce and fold

--- app/src/main/java/com/example/kotlinflow/MainViewModel.kt
+++ app/src/main/java/com/example/kotlinflow/MainViewModel.kt
@@ -3,9 +3,7 @@ package com.example.kotlinflow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
@@ -26,6 +24,16 @@ class MainViewModel: ViewModel() {
             countDownFlow.collect { time ->
                 println("The current time is $time")
             }
+
+            // accumulator -> sum of previous values, value -> present result
+           /* val reduceResult = countDownFlow.reduce { accumulator, value ->
+                accumulator + value
+            }*/
+
+            // is like reduce, but I need to provide a start value
+            /*val foldResult = countDownFlow.fold(100) { accumulator, value ->
+                accumulator + value
+            }*/
         }
     }
 }
